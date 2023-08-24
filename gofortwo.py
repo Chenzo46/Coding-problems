22716cff41270966110133532245d34d618b26b3
Chenzo46 <deliguorilorenzo@gmail.com>
2023-08-23 21:36:24 -0400

    go for two solved

--- gofortwo.py
+++ gofortwo.py
@@ -1,5 +1,19 @@
 def main():
-    pass
+    
+    cases = int(input())
+    #0-14 down, 16-27 up, 0 diff at 15
+    special_cases = [2,1,2,1,2,2,1,2,1,1,2,2,1,2,1,1,2,1,1,1,2,1,1,1,1,1,1,2]
+    for _ in range(cases):
+
+        team, opp = list(map(int, input().split()))
+
+        diff = team - opp
+        dist = diff + 15
+        
+        if dist in range(0,27):
+            print(special_cases[dist])
+        else:
+            print(1)
 
 if __name__ == '__main__':
     main()
