25b06c923c3014c81a9e04d67523f3074a8bc430
Chenzo46 <deliguorilorenzo@gmail.com>
2023-08-24 12:36:15 -0400

    Natrual boost complete

--- natrualboost.py
+++ natrualboost.py
@@ -1,6 +1,13 @@
+from math import cos, pi, radians
+
 earth_radius = 6_370_000
 cases = int(input())
 
 for _ in range(cases):
+    theta = float(input())
 
-    pass
+    n_radius = earth_radius * cos(radians(theta))
+    circumfrence = n_radius * 2  * pi
+    velocity = circumfrence / 86_400
+
+    print(int(velocity))
